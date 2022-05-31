31e04e10360f648ab0eb1d41fb7aa13bf2a89631
Ken <info@ken89.com>
2022-05-31 15:23:01 +0900

    add accordion content advanced settings

--- script/main.js
+++ script/main.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+const accordion = document.getElementsByClassName('accordion-label');
+
+for (i=0; i<accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+        } else {panel.style.maxHeight = panel.scrollHeight + 'px';
+    }
+  })
+}
